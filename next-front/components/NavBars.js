aec7b04534e61e4353076109653e75e204a6ba35
Ralph <pok12369@naver.com>
2022-11-25 06:49:43 +0900

    i learned Next style

--- next-front/components/NavBars.js
+++ next-front/components/NavBars.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
     console.log(router);
     return (
         <nav>
-            <Link href="/" style={{color: router.pathname === "/" ? "red" : ""}}>
+            <Link href="/" className={`${styles.link} ${router.locale === "/" ? styles.active : ""}`}>
                 Home
             </Link>
-            <Link href="/about" style={{color: router.pathname === "/about" ? "red" : ""}}>
+            <Link href="/about" className={`${styles.link} ${router.locale === "/about" ? styles.active : ""}`}>
                 About
             </Link>
         </nav>
